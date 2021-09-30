ac329030bb9db611dfcb5a098d1b7daa59f4a717
Your Name <you@example.com>
2021-09-30 18:06:26 -0400

    Greet

--- 08_hellofxn/Greet1.java
+++ 08_hellofxn/Greet1.java
@@ -1,5 +1,11 @@
 public class Greet {
-  public static void main(String[] args) {
-    System.out.println("No hablo queso");
-  }
+	public static void main(String[] args) {
+		Greet("person 1");
+		Greet("person 2");
+		Greet("person 3");
+	}
+	
+	public static String greet(String) {
+		System.out.println("Why, hello there, " + String + ". How do you do?");
+	}
 }
