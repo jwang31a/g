4a229818569774b5b9a93d3701ff80c08d3b093c
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-09-30 18:18:20 -0400

    Update Greet1.java

--- 08_hellofxn/Greet1.java
+++ 08_hellofxn/Greet1.java
@@ -7,8 +7,11 @@ public class Greet1 {
 	
 	public static String greet(String string) {
 		String output = "";
+		// I think the system of creating a new variable and defining it is notable, because it's different from other languages. 
+		// In Python, the creation and definition of a variable could be done without stating what type of variable it is. and it was always done on one line.
 		output = "Why, hello there, " + string + ". How do you do?";
 		System.out.println(output);
 		return(output);
+		// I think that something notable is that the return statement doesn't also print the text (I could be wrong here, but I remember Python did it).
 	}
 }
