80345b672f3f744129b7f33175c49b408dd39b0a
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-02 12:27:39 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,12 +1,13 @@
 public class Greet {
-  public static void main(String[] args) {
-    greet("Flim");
-    greet("Flam");
-    greet("Kazaam");
-  }
-  public static String greet(String string) {
+ 	public static void main(String[] args) {
+   	greet("Flim");
+   	greet("Flam");
+   	greet("Kazaam");
+ 	}
+ 	public static String greet(String string) {
 		String output = "";
 		output = "Why, hello there, " + string + ". How do you do?";
 		System.out.println(output);
 		return(output);
+ 	}
 }
