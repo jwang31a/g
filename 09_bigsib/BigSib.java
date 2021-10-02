9d8254f8c290798a43e9ce6e05139254e3f49aef
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-02 12:08:41 -0400

    Update and rename Bigsib.java to BigSib.java

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+public class BigSib {
+  public static void main(String[] args) {
+    
+  }
+  
+  public static String greet(String string) {
+		String output = "";
+		output = "Why, hello there, " + string + ". How do you do?";
+		System.out.println(output);
+		return(output);
+	}
+}
