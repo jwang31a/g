2489b16e6b720a26cc088b42e1f14ca63acc8eee
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-02 12:32:27 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,7 +1,5 @@
 public class Greet {
- 	public static void main(String[] args) {
-   	greet("Flim");
-   	greet("Flam");
-   	greet("Kazaam");
- 	}
+	public static void main(String[] args) {
+	
+	}
 }
