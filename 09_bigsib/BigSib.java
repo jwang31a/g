d8e8771b3d492d301a86a1686e75e2c9c77fc389
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-02 12:33:11 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,7 +1,7 @@
 public class BigSib {
 	public static String greet(String string) {
 		String output = "";
-		output = "Why, hello there, " + string + ". How do you do?";
+		output = "Why, hello there, " + string + ". How do you do";
 		System.out.println(output);
 		return(output);
 	}
