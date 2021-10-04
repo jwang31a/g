6c39a90fd51459141957256c7cb5c810da1c91b1
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-03 21:57:39 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -4,5 +4,6 @@ public class BigSib {
 		output = "Why, hello there, " + string + ". How do you do?";
 		System.out.println(output);
 		return(output);
+		// So this code is fairly intuitive, as it is a copy of old code, so there's nothing new here.
 	}
 }
