2d0d8e0f449da53dd98a2c98a8fc7c92c86bd0f8
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-03 22:02:45 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -5,5 +5,7 @@ public class BigSib {
 		System.out.println(output);
 		return(output);
 		// So this code is fairly intuitive, as it is a copy of old code, so there's nothing new here.
+		// The only weird thing about this file is that there is main method.
+		// I didn't know that this could run without a main method.
 	}
 }
