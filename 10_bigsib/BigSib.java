4653eb937738eb867730bdb23bcc4bd3162d5764
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-04 17:47:50 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -2,7 +2,6 @@ public class BigSib {
 	public static String greet(String string) {
 		String output = "";
 		output = "Why, hello there, " + string + ". How do you do?";
-		System.out.println(output);
 		return(output);
 		// So this code is fairly intuitive, as it is a copy of old code, so there's nothing new here.
 		// The only weird thing about this file is that there is main method.
