8272caeccc42ba25b87d0b12a388c0b74926b837
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-05 16:47:55 -0400

    Update Greet.java

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+	public static void main(String[] args) {
+		System.out.println(BigSib.greet("Flim"));
+		System.out.println(BigSib.greet("Flam"));
+		System.out.println(BigSib.greet("Kazaam"));
+		// When I was testing this, I'm not sure why it didn't work, but then it suddenly worked.
+		// I don't know what I changed, but I made this part work somehow.
+		
+		// Pretty much nothing changed here, other than that I added the print statement here.
+	}
+}
