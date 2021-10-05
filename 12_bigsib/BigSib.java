357c2c14d3682b374d45b05dfb62356b0063ab67
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-05 17:08:10 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,12 +1,14 @@
 public class BigSib {
-	public static void main(String[] args) {
-		greet("freshman");
-	}
-	
 	public static String greet(String string) {
 		String output = "";
 		output = string;
 		System.out.println(output);
+		return(output);
+	}
+	
+	public static String setHelloMsg(String string) {
+		String output = "";
+		output = string;
 		return(output);
 	}
 }
