8da6d5cd9242991b965a3e3469cf531ddbf7b188
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-05 16:51:26 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,7 +1,11 @@
 public class Greet {
 	public static void main(String[] args) {
-		System.out.println(BigSib.greet("Flim"));
-		System.out.println(BigSib.greet("Flam"));
-		System.out.println(BigSib.greet("Kazaam"));
+		String greeting;
+		
+		BigSib richard = new BigSib();
+		richard.setHelloMsg("Word up");
+		
+		greeting = richard.greet("freshman");
+		System.out.println(greeting);
 	}
 }
