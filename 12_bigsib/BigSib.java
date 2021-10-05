667acebed274269b4ef8961e6ae3f3ac6b5d09f0
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-05 16:47:31 -0400

    Update BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,14 @@
+public class BigSib {
+	public static String greet(String string) {
+		String output = "";
+		output = "Why, hello there, " + string + ". How do you do?";
+		return(output);
+		// So this code is fairly intuitive, as it is a copy of old code, so there's nothing new here.
+		// The only weird thing about this file is that there is main method.
+		// I didn't know that this could run without a main method.
+		
+		// New comments:
+		// So I think I did hw#9 "wrong", as in i pretty much did exactly what I was supposed to do in hw#10 (take this code out of a string method)
+		// Pretty much nothing changed here, other than the removal of a print statement.
+	}
+}
