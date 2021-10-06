67c0d8f11378f87ac47f49ac091cf58383c5533a
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-06 18:52:50 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -2,10 +2,8 @@ public class Greet {
  	public static void main( String[] args ){
  		String greeting;
  		BigSib richard = new BigSib();
-		// Still a bit confused as to what richard is, but I think it just takes the place of BigSib here?
 
 		richard.setHelloMsg("Word up");
-		// This sets the variable HelloMsg to whatever string is typed in here
 		greeting = richard.greet("freshman");
 		System.out.println(greeting);
 		
