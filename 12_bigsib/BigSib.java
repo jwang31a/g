8ca6e769a2f0874b9ff30a6e47dea6a739565c8d
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-05 20:29:52 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -4,21 +4,21 @@
  		BigSib richard = new BigSib();
 		// Still a bit confused as to what richard is, but I think it just takes the place of BigSib here?
 
-        richard.setHelloMsg("Word up");
+		richard.setHelloMsg("Word up");
 		// This sets the variable HelloMsg to whatever string is typed in here
-        greeting = richard.greet("freshman");
-        System.out.println(greeting);
+		greeting = richard.greet("freshman");
+		System.out.println(greeting);
+		
+		richard.setHelloMsg("Salutations");
+		greeting = richard.greet("Dr. Spaceman");
+		System.out.println(greeting);
 
-        richard.setHelloMsg("Salutations");
-        greeting = richard.greet("Dr. Spaceman");
-        System.out.println(greeting);
+		richard.setHelloMsg("Hey ya");
+		greeting = richard.greet("Kong Fooey");
+		System.out.println(greeting);
 
-        richard.setHelloMsg("Hey ya");
-        greeting = richard.greet("Kong Fooey");
-        System.out.println(greeting);
-
-        richard.setHelloMsg("Sup");
-        greeting = richard.greet("mom");
-        System.out.println(greeting);
-    }
+		richard.setHelloMsg("Sup");
+		greeting = richard.greet("mom");
+		System.out.println(greeting);
+	}
 }
