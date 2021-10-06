3868eb2c88c3b4e19b4ee52b125e966ecb0344fb
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-06 18:52:34 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,8 +1,5 @@
 public class BigSib {
 	String HelloMsg = "";
-	// I thought that things couldn't exist/be defined outside of a method, but we can do it.
-	// This changes things, because every method in this class can access this variable, and this be very helpful.
-	// I kind of threw out a lot of my Python knowledge, because I thought that Java was pretty differend, but I think that information would have saved a lot of time for me.
 	public String greet(String string) {
 		String output = "";
 		output = HelloMsg + " " + string;
@@ -12,6 +9,5 @@ public class BigSib {
 	public String setHelloMsg(String string) {
 		HelloMsg = string;
 		return(HelloMsg);
-		// The point of this method is to set the value of a variable, which is how we can use this variable in greet and get a valid answer.
 	}
 }
