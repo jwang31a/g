8741eda17026f28dd67b5cbf3756c475db5c5e5a
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-06 19:08:26 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,13 +1,19 @@
 public class BigSib {
-	String HelloMsg = "";
+	private String HelloMsg1 = "";
+	private String HelloMsg2 = "";
+	private String HelloMsg3 = "";
+	private String HelloMsg4 = "";
+	
+	public BigSib() {
+		HelloMsg1 = "Word up";
+		HelloMsg2 = "Salutations";
+		HelloMsg3 = "Hey ya";
+		HelloMsg4 = "Sup";
+	}
+	
 	public String greet(String string) {
 		String output = "";
 		output = HelloMsg + " " + string;
 		return(output);
-	}
-	
-	public String setHelloMsg(String string) {
-		HelloMsg = string;
-		return(HelloMsg);
 	}
 }
