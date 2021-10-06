13dd6c595f3214aee4e7e64ea012f0449ee97f9f
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-05 20:10:37 -0400

    Update Test.java

--- 12_bigsib/Test.java
+++ 12_bigsib/Test.java
@@ -1,12 +1,22 @@
-public class Test {
-	public static void main(String[] args) {
-		greet("freshman");
-	}
-	
-	public static String greet(String string) {
-		String output = "";
-		output = string;
-		System.out.println(output);
-		return(output);
-	}
+ public class Test {
+    public static void main( String[] args ){
+        String greeting;
+        BigSib richard = new BigSib();
+
+        richard.setHelloMsg("Word up");
+        greeting = richard.greet("freshman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Salutations");
+        greeting = richard.greet("Dr. Spaceman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Hey ya");
+        greeting = richard.greet("Kong Fooey");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Sup");
+        greeting = richard.greet("mom");
+        System.out.println(greeting);
+    }
 }
