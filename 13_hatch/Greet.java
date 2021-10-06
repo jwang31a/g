c88aaa755fcf46d2456da5333aca217491f15eb1
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-06 19:15:02 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,3 +1,14 @@
+//Team Triple J, Jefford, Jeffrey, Jun Hong; (turtles) Dylan, Mathias, Bob
+//APCS
+//HW#13_hatch, Where do BigSibs Come From?, 
+//use constructor (special method that runs automatically at the beginning) to make Greet.java run without setHelloMsg.
+//2021-10-06
+
+//Discoveries:
+//Constructor method is very useful, and will help with condensing code down.
+
+//QCC
+
 public class Greet {
  	public static void main( String[] args ){
  		String greeting;
