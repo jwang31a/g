fc757cdde525649b637bff203496397160a3e4a3
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 19:55:27 -0400

    Update Greet.java

--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -13,7 +13,7 @@ QCC
 public class Greet {
  	public static void main( String[] args ){
  		String greeting;
-		BigSib richard = new BigSib();
+		BigSib richard = new BigSib("Word up","Salutations","Hey ya","Sup");
 		
  		BigSib richard1 = new BigSib(0,"freshman");
 		greeting = richard1.output;
