49fd24102e3937a38c13627bd8b7fce5d24ec05c
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 19:05:57 -0400

    Update Greet.java

--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -15,19 +15,19 @@ public class Greet {
  		String greeting;
 		BigSib richard = new BigSib();
 		
- 		BigSib richard1 = new BigSib(1,"Word up");
+ 		BigSib richard1 = new BigSib(0,"Word up");
 		greeting = richard1.output;
 		System.out.println(greeting);
 		
-		BigSib richard2 = new BigSib(2,"Salutations");
+		BigSib richard2 = new BigSib(1,"Salutations");
 		greeting = richard2.output;
 		System.out.println(greeting);
 
-		BigSib richard3 = new BigSib(3,"Hey ya");
+		BigSib richard3 = new BigSib(2,"Hey ya");
 		greeting = richard3.output;
 		System.out.println(greeting);
 
-		BigSib richard4 = new BigSib(4,"Sup");
+		BigSib richard4 = new BigSib(3,"Sup");
 		greeting = richard4.output;
 		System.out.println(greeting);
 	}
