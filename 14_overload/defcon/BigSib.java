ea49a8d6cec287a431dcbee354bd6616fb4a38bc
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 17:23:49 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -1,1 +1,13 @@
-
+public class BigSib {
+	public String HelloMsg = "";
+	
+	public BigSib(String input) {
+		HelloMsg = input;
+	}
+	
+	public String greet(String string) {
+		String output = "";
+		output = HelloMsg + " " + string;
+		return(output);
+	}
+}
