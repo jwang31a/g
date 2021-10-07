e23a2f95d7ceb4adc7850ed6486b8b9d1a7f0e3b
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 19:31:32 -0400

    Create Test.java

---
+++ 14_overload/overcon/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+  private String HelloMsg1 = "";
+	private String HelloMsg2 = "";
+	private String HelloMsg3 = "";
+	private String HelloMsg4 = "";
+	  
+	public BigSib() {
+	HelloMsg1 = "Word up";
+	HelloMsg2 = "Salutations";
+  HelloMsg3 = "Hey ya";
+  HelloMsg4 = "Sup";
+	}
+  public static void main(String[] args) {
+    System.out.println(HelloMsg1);
+    System.out.println(HelloMsg1);
+    System.out.println(HelloMsg1);
+    System.out.println(HelloMsg1);
+  }
+}
