fd4a21de89fe27c8e04264393b495e849bf3bc73
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 18:45:47 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -1,3 +1,15 @@
+/*
+Team Triple J, Jefford, Jeffrey, Jun Hong; (turtles) Dylan, Mathias, Bob
+APCS
+HW#14_overload, Customize Your Creation, 
+use default and overloaded constructor to set the value for a variable.
+2021-10-07
+
+Discoveries:
+
+QCC
+*/
+
 public class BigSib {
 	public String HelloMsg1 = "";
 	public String HelloMsg2 = "";
