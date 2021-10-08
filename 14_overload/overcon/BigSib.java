6eebc333ea597db9c73ca5a41c81f808aa53b4b6
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 20:22:21 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -11,14 +11,22 @@ QCC
 */
 
 public class BigSib {
-	private String HelloMsg;
+	private String HelloMsg1;
+	private String HelloMsg2;
+	private String HelloMsg3;
+	private String HelloMsg4;
+	private String[] list = {HelloMsg1,HelloMsg2,HelloMsg3,HelloMsg4};
 	public String output = "";
 	
-	public BigSib(String string) {
-		HelloMsg = string;
+	public BigSib(String one, String two, String three, String four) {
+		HelloMsg1 = one;
+		HelloMsg2 = two;
+		HelloMsg3 = three;
+		HelloMsg4 = four;
 	}
 	
-	public BigSib(String string) {
-		output = HelloMsg + " " + string;
+	public BigSib(int input, String string) {
+		System.out.println(list[input]);
+		output = list[input] + " " + string;
 	}
 }
