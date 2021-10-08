ec78e28ac478ce2314f42124569fd6e12230bc26
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-07 20:21:04 -0400

    Update Backup.java

--- 14_overload/overcon/Backup.java
+++ 14_overload/overcon/Backup.java
@@ -1,10 +1,17 @@
 public class BigSib {
-	private String HelloMsg1 = "Word up";
-	private String HelloMsg2 = "Salutations";
-	private String HelloMsg3 = "Hey ya";
-	private String HelloMsg4 = "Sup";
+	private String HelloMsg1;
+	private String HelloMsg2;
+	private String HelloMsg3;
+	private String HelloMsg4;
 	private String[] list = {HelloMsg1,HelloMsg2,HelloMsg3,HelloMsg4};
 	public String output = "";
+	
+	public BigSib(String one, String two, String three, String four) {
+		HelloMsg1 = one;
+		HelloMsg2 = two;
+		HelloMsg3 = three;
+		HelloMsg4 = four;
+	}
 	
 	public BigSib(int input, String string) {
 		System.out.println(list[input]);
