03ab0a202ab62e1ef1f71804e8c69e1ae1adc221
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-17 11:36:59 -0400

    Create BankAccount.java

---
+++ 18_BankAccount/BankAccount.java
@@ -0,0 +1,70 @@
+/**
+   Clyde "Thluffy" Sinclair
+   APCS pd0
+   HW18 -- building a more meaningful class
+   2021-10-18
+   
+   instance variables for storing...
+   account holder's full name
+   account password
+   4-digit PIN
+   9-digit account number
+   account balance
+   
+   and methods for...
+   setting each attribute
+   printing out all of an account’s info at once
+   depositing money
+   withdrawing money
+**/
+
+public class BankAccount {
+
+  //instance variables
+  //---------------------------------------------
+
+
+  //mutators
+  public String setName( String newName ) {
+    // INSERT YOUR CODE HERE
+  }
+
+  public String setPasswd( String newPasswd ) {
+    // INSERT YOUR CODE HERE
+  }
+
+  public short setPin( short newPin ) {
+    // INSERT YOUR CODE HERE
+  }
+
+  public int setAcctNum( int newAcctNum ) {
+    // INSERT YOUR CODE HERE
+  }
+
+  public double setBalance( double newBalance ) {
+    // INSERT YOUR CODE HERE
+  }
+  //---------------------------------------------
+
+
+  public void deposit( double depositAmount ) {
+    // INSERT YOUR CODE HERE
+  }
+
+  public void withdraw( double withdrawAmount ) {
+    // INSERT YOUR CODE HERE
+  }
+
+
+  //overridden toString()
+  public String toString() {
+    // INSERT YOUR CODE HERE
+  }
+
+
+  //main method for testing
+  public static void main( String[] args ) {
+    // INSERT YOUR METHOD CALLS FOR TESTING HERE
+  }//end main()
+
+}//end class BankAccount
