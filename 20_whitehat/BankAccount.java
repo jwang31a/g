21aca8d96cf2e7fc31524a2bc0e7cbcdc10b6412
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-19 21:49:28 -0400

    Update BankAccount.java

--- 20_whitehat/BankAccount.java
+++ 20_whitehat/BankAccount.java
@@ -93,35 +93,4 @@ public boolean authenticate( int accNum, String pass ) {
     return (false);
   }
 }
-
-  //main method for testing
-  public static void main( String[] args ) {
-    BankAccount ba = new BankAccount();
-      System.out.println(ba.toString());
-  //prints errors
-    
-    short badPin = 0100;
-    ba.setName("badPepe");
-    ba.setPasswd("PepeIsBad");
-    ba.setPin(badPin);
-    ba.setAcctNum(000000001);
-    System.out.println(ba.toString());
-    ba.setBalance(1000.00);
-    ba.deposit (200.00);
-    ba.withdraw(10000.99);
-    System.out.println("Authentication " + ba.authenticate(123456789, "PepeIsGod"));
-    
-//prints full info
-  short pin = 1234;
-    ba.setName("Pepe");
-    ba.setPasswd("PepeIsGod");
-    ba.setPin(pin);
-    ba.setAcctNum(123456789);
-    System.out.println(ba.toString());
-    ba.setBalance(10000000.00);
-    ba.deposit(-200000.00);
-    ba.withdraw(200.00);
-    System.out.println("Authentication " + ba.authenticate(123456789, "PepeIsGod"));
-}//end main()
-
 }//end class BankAccount
