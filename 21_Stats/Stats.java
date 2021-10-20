628bd9e55d12f0cbca795d36795318152123e22c
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-20 18:11:43 -0400

    Update Stats.java

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -11,34 +11,42 @@
  **/
 
 public class Stats {
-
-  public static int mean(int a, int b) {
-	  int output = (a + b) / 2;
-	  return output;
-  }
+	public static int mean(int a, int b) {
+		int output = (a + b) / 2;
+		return output;
+  		}
   
-  public static double mean(double a, double b) {
-	  double output = (a + b) / 2;
-	  return output;
-  }
+	public static double mean(double a, double b) {
+		double output = (a + b) / 2;
+		return output;
+	}
 	
+	public static int max(int a, int b) {
+		int output;
+		if (a > b) {
+			output = a;
+		} else if (a < b) {
+			output = b;
+		} else (a == b) {
+			output = a;
+		}
+		return output;
+	}
 /**
-  public static int max(int a, int b) {
-  }
-  public static double max(double a, double b) {
-  }
-  public static int geoMean(int a, int b) {
-  }
-  public static double geoMean(double a, double b) {
-  }
-  public static int max(int a, int b, int c) {
-  }
-  public static double max(double a, double b, double c) {
-  }
-  public static int geoMean(int a, int b, int c) {
-  }
-  public static double geoMean(double a, double b, double c) {
-  }
+ 	public static double max(double a, double b) {
+ 	}
+ 	public static int geoMean(int a, int b) {
+ 	}
+ 	public static double geoMean(double a, double b) {
+ 	}
+ 	public static int max(int a, int b, int c) {
+ 	}
+ 	public static double max(double a, double b, double c) {
+ 	}
+ 	public static int geoMean(int a, int b, int c) {
+ 	}
+ 	public static double geoMean(double a, double b, double c) {
+ 	}
 **/
 
   //main method for testing functionality
@@ -46,7 +54,10 @@ public class Stats {
 	  // should output 1.5, but it seems like it rounds down?
 	  System.out.println(Stats.mean(1,2));
 	  
-	  // should output 1.5
+	  // should output 1.5, outputs correct mean
 	  System.out.println(Stats.mean(1.0,2.0));
+	  
+	  // should return the bigger of the two numbers
+	  System.out.println(Stats.max(1,100))
   }
 }//end class
