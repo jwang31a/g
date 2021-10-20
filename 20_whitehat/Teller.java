8a119dae2bef59445804f9e3f924a946882b3ac2
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-19 21:47:54 -0400

    Update Teller.java

--- 20_whitehat/Teller.java
+++ 20_whitehat/Teller.java
@@ -3,4 +3,40 @@
   APCS pd6
   HW20 -- External Audit
   2021-10-19
+  
+  DISCO:
+  
+  QCC:
+  
 **/
+
+public class Teller {
+  public static void main( String[] args ) {
+    BankAccount ba = new BankAccount();
+    System.out.println(ba.toString());
+  //prints errors
+    
+    short badPin = 0100;
+    ba.setName("badPepe");
+    ba.setPasswd("PepeIsBad");
+    ba.setPin(badPin);
+    ba.setAcctNum(000000001);
+    System.out.println(ba.toString());
+    ba.setBalance(1000.00);
+    ba.deposit (200.00);
+    ba.withdraw(10000.99);
+    System.out.println("Authentication " + ba.authenticate(123456789, "PepeIsGod"));
+    
+//prints full info
+  short pin = 1234;
+    ba.setName("Pepe");
+    ba.setPasswd("PepeIsGod");
+    ba.setPin(pin);
+    ba.setAcctNum(123456789);
+    System.out.println(ba.toString());
+    ba.setBalance(10000000.00);
+    ba.deposit(-200000.00);
+    ba.withdraw(200.00);
+    System.out.println("Authentication " + ba.authenticate(123456789, "PepeIsGod"));
+  }
+}
