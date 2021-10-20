9201eafd3a3ab363af8b97f18c1f8e3b42df18ee
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-19 22:02:31 -0400

    Update Teller.java

--- 20_whitehat/Teller.java
+++ 20_whitehat/Teller.java
@@ -10,6 +10,8 @@
   
   QCC:
   The toString method that prints out all of the account information feels like it's in the wrong place, and it might print out outdated information.
+  A common issue between BankAccounts is that we don't account for depositing negative numbers, and this pretty much allows us to steal from accounts. 
+  In the first account, we were able to empty the bank account using this.
   
   What operations are possible from BankAccount's main method but not Teller's?
   Initially, authenticate() was a private method, only accessible from BankAccount.java, but we changed it to public so it could be accessed here.
@@ -27,7 +29,7 @@ public class Teller {
     ba.setPin(badPin);
     ba.setAcctNum(000000001);
     ba.setBalance(100000000);
-    ba.deposit (-100000000);
+    ba.deposit (-200000000);
     ba.withdraw(0);
     System.out.println("Authentication " + ba.authenticate(0000000001, "PepeIsBad"));
     System.out.println(ba.toString());
