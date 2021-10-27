f9049751e4196eeb31caec6a25b6ac6a6e3be08e
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-26 20:03:13 -0400

    Create Repeater.java

---
+++ 25_repeat/Repeater.java
@@ -0,0 +1,39 @@
+public class Repeater{
+  String output="|";
+  String fenceW(int numPosts){
+  if(numPosts-1==0){
+    System.out.println(output);
+  }
+  else{
+    if (numPosts != 0){
+      output+="--|";
+      fenceW(n-1);
+    }
+    else{
+      output=" ";
+      return output;
+     }
+    }
+  }
+  
+  String fenceR(int numPosts){
+    while (numPosts>1){
+      output+="|--";
+      numPosts-=1;
+    }
+    output+="|";
+    if (numPosts==0){
+      output=" " ;
+    }
+    return output;
+  }
+
+  public static void main (String [] args){
+    fenceW(0);
+    fenceW(1);
+    fenceW(2);
+    fenceR(0);
+    fenceR(1);
+    fenceR(2);
+   }
+}
