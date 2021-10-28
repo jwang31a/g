9a9d83b7908873ebc75a590f47d5038cdd9665c8
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-27 22:10:13 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -79,6 +79,6 @@ public class Stats {
 	public static void main(String[] args) {
 		System.out.println(gcd(6,8));
 		//System.out.println(gcdER(6,8));
-		System.out.println(gcdEW(6,8));
+		System.out.println(gcdEW(8,8));
 	}
 }
