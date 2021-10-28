c75505360df6d70ef57db5bf8600d753de0c843a
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-27 21:18:00 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -1,19 +1,20 @@
 /**
-	Team NBJ: Nada, Brian K., Jun Hong
-  APCS PD6
-  HW26: GCD Three Ways
-  2021-10-27
-	
-  DISCO:
+Team NBJ: Nada, Brian K., Jun Hong
+APCS PD6
+HW26: GCD Three Ways
+2021-10-27
+25 minutes as of 9:17
 
-	QCC:
+DISCO:
+
+QCC:
 	
-	ALGO:
+ALGO:
 
  **/
 
 public class Stats {
-	//brute force method
+	//brute force method, using while loop
 	public static int gcd(int a, int b) {
 		int output = 1;
 		int d = 1;
@@ -27,8 +28,24 @@ public class Stats {
 		return output;
 	}
 	
+	//recursive method
+	public static int gcdER(int a, int b) {
+		if (a == b) {
+			return(a);
+		} 
+		if (a < b) {
+			if (b % a == 0) {
+				return(a);
+			}
+		} else {
+			if (a % b = 0 ) {
+				return(b);
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		System.out.println(gcd(6,8));
+		System.out.println(gcdER(6,8));
 	}
 }
