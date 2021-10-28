c7a83d7729448f89df2325ad6aefff60e1cad067
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-27 20:53:14 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -1,5 +1,34 @@
+/**
+	Team NBJ: Nada, Brian K., Jun Hong
+  APCS PD6
+  HW26: GCD Three Ways
+  2021-10-27
+	
+  DISCO:
+
+	QCC:
+	
+	ALGO:
+
+ **/
+
 public class Stats {
-	public static void main(String[] args) {
+	//brute force method
+	public static int gcd(int a, int b) {
+		int output = 1;
+		int d = 1;
 		
+		while (d != a || d != b) {
+			if (a % d == 0 && b % d == 0) {
+				output = d
+			}
+			d++;
+		}
+		return output;
+	}
+	
+	
+	public static void main(String[] args) {
+		gcd(16,32);
 	}
 }
