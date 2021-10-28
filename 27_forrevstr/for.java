90de26a5fb484c041ad63dd2c641f2a08359ea38
Jun Hong Wang <jwang31@stuy.edu>
2021-10-28 16:29:51 -0400

    for work

---
+++ 27_forrevstr/for.java
@@ -0,0 +1,26 @@
+public class for {
+	public String fenceF(int posts) {
+		String result = "|";
+		for (int postCounter = 1; postCounter < posts; postCounter++) {
+			result += "--|"
+		}
+		return result;
+	}
+
+	public String reverseF(String s) {
+		String result = "";
+		for (int character = s.length()-1; character>=0;character--){
+			result+=s[character];
+		}
+		return result;
+		
+	}
+
+	public String reverseR(String s){
+		if (s.length()==1){
+			return s
+		}
+		return reverseR(s.substring(1))+s[0];
+	}
+
+}
