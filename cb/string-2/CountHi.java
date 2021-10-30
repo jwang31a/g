aa662a6dfdd70e97d481d8d437e7c51c50dd57cb
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-29 23:16:03 -0400

    Update CountHi.java

--- cb/string-2/CountHi.java
+++ cb/string-2/CountHi.java
@@ -1,4 +1,5 @@
 public class CountHi {
+  //counts number of times String "hi" has appeared
   public int countHi(String str) {
     int output = 0;
     for (int index = 0; index + 2 <= str.length(); index++) {
