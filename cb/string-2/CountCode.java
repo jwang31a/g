08e0567151f6e413af307f7b02adfaba308130b2
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-29 23:20:14 -0400

    Create CountCode.java

---
+++ cb/string-2/CountCode.java
@@ -0,0 +1,19 @@
+public class CountCode {
+  //counts the number of times String "code" appears, but the third letter doesn't matter
+  public static int countCode(String str) {
+    int counter = 0;
+    for (index = 0; index + 4 <= str.length(); index++) {
+      s = str.substring(index, index + 4);
+      if (s.substring(0,3).equals("co") && s.substring(3,4).equals("e")) {
+        counter++
+      }
+    }
+    return counter;
+  }
+  
+  public static void main(String[] args) {
+    countCode("aaacodebbb");
+    countCode("codexxcode");
+    countCode("cozexxcope");
+  }
+}
