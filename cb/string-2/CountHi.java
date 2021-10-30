19dd051671d0438d2335e62916e0fb0e26a3793f
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-10-29 23:08:44 -0400

    Create CountHi.java

---
+++ cb/string-2/CountHi.java
@@ -0,0 +1,11 @@
+public class CountHi {
+  public int countHi(String str) {
+    int output = 0;
+    for (int index = 0; index + 2 <= str.length(); index++) {
+      if (str.substring(index, index + 2).equals("hi")) {
+        output++;
+      }
+    }
+    return output;
+  }
+}
