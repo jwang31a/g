31e512dcfdbc4f9b68f11bbff6ba7e73e369ef30
Your Name <you@example.com>
2021-11-02 13:24:53 -0400

    for each loop

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -10,6 +10,7 @@ I have a much better understanding of recursion in java now.
 Instead of returning a recursive call only, we have to return something + a recursive call, so that when we reach the base case, we get all the outputs from before as well.
 I also found out that Integer.parseInt() isn't a perfect method, since it gets rid of leading zeros.
 For example, when I try Integer.parseInt("0000000000"), the zeros get removed, and it returns 0.
+I understand how to do a for each loop now so that I can input arguments via the command line.
 
 QCC:
 The code is definitely very inefficient, since I didn't really think about simplifiying the code.
@@ -20,19 +21,19 @@ The method only works in the first digit of a set (so something like ",000" or "
 However, the iterative one works perfectly fine.
 
 I'm not sure how to do the for each loop such that I can input via the command line.
-
+This has been fixed now, I I understand this a lot more.
+Since this is in the main method, the array args is a String array, so all inputs will be interpreted as Strings, so we have to convert it from a String into an int for the method to work.
 */
 
 public class Commafier {
   public static void main(String[] args) {
 
     /*explicit calls, commented out
-
     System.out.println(commafyR(1));
     System.out.println(commafyR(10));
     System.out.println(commafyR(100));
     System.out.println(commafyR(1000));
-    System.out.println(commafyR(12345));
+    System.out.println(commafyR(123450000));
 
     System.out.println("\n");
 
@@ -45,17 +46,17 @@ public class Commafier {
     System.out.println("\n");
     */
 
-    int[] arg = {};
-    for (int i: arg) {
-      System.out.println(commafyR(i));
-      System.out.println(commafyF(i));
+    for (String i: args) {
+      System.out.println(commafyR(Integer.parseInt(i)));
+      System.out.println(commafyF(Integer.parseInt(i)));
     }
   }
 
   //recursive commafy, done slightly differently than the iterative one
-  //the problem here is that Integer.parseInt("000") returns 0, not 000.
+  //the problem here is that Integer.parseInt("000") returns 0, not 000. (this problem has been fixed)
   //Is there a way to convert from a String to int that preserves the 0?
   //I could write a helper method, but I'm not sure if that's allowed, or if it preserves the leading 0s.
+  //this is working now
 
   public static String commafyR(int n) {
     String input = Integer.toString(n);
@@ -77,7 +78,7 @@ public class Commafier {
       return output + commafyR(newInput);
     } else if (input.length() % 3 == 0 && input.length() >= 3) {
       output = "," + input.substring(input.length() - 3, input.length());
-      newInput = Integer.parseInt(input.substring(0));
+      newInput = Integer.parseInt(input.substring(0, input.length()-3));
       return commafyR(newInput) + output;
     }
 
