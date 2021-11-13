0a81138a512094b66adaa884670a909dcf3ed101
Your Name <you@example.com>
2021-11-12 22:55:03 -0500

    array

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -27,15 +27,25 @@ public class Loopier {
     return -1;
   }
 
-  /*
-  under construction
   public static int linSearchR(int[] a, int target) {
-    int[] array = new int[a.length - 2];
-    if (a[0] == target) {
-      return
+    int index = a.length - 1;
+    int[] array = new int[a.length - 1];
+    if (a[a.length-1] != target && a.length > 1) {
+      for (int i = array.length-1; i >0; i-=1){
+        array[i] = a[i];
+      }
+      return linSearchR(array, target);
+    } else if (a[a.length-1] == target){
+      for (int i = array.length-1; i >0; i-=1){
+        array[i] = a[i];
+      }
+      return linSearchR(array, target);
+    } else if (index >= 0){
+      return index;
+    } else {
+      return -1;
     }
   }
-  */
 
   public static int freq(int[] a, int target) {
     int count = 0;
@@ -47,23 +57,27 @@ public class Loopier {
     return count;
   }
 
-  /*
-  under construction
-  public static int freqR(int[] a, int target) {
-    int count = 0;
-    if (a[i] == target) {
-      count++;
+  public static int[] freqR(int[] a, int target) {
+    int[] array = new int[a.length - 1];
+
+    for (int i = 0; i <= array.length - 1; i++) {
+      array[array.length - (1 + i)] = a[i];
     }
-    return(count);
+    return array;
+
+    //if (a[a.length - 1] == target) {
+
+    //}
   }
-  */
+
 
   public static void main(String[] args) {
     int[] a = new int[7];
     int[] apple = {1,2,4,5,3,6,7,2,1};
     System.out.println(stringify(arrayGen(a)));
     System.out.println(linSearch(apple, 3));
-    //System.out.println(linSearchR(apple, 5));
+    System.out.println(linSearchR(apple, 5));
     System.out.println(freq(apple, 4));
+    System.out.println(stringify(freqR(apple, 4)));
   }
 }
