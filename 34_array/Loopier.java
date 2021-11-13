973f1704c9c4229756ae49cf244356836ae3fc8f
Your Name <you@example.com>
2021-11-12 20:49:23 -0500

    g

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -28,19 +28,45 @@ public class Loopier {
   }
 
   public static int linSearchR(int[] a, int target) {
-    int[] array = a[0,a.length-2];
+    int[] array = new int[a.length - 2];
     if (a[a.length-1] == target){
       return a.length-1;
     }
     else {
+      for (int i = 1; i <= a.length -2; i++) {
+        array[i] = a[i];
+      }
       return linSearchR(array, target);
     }
   }
 
+  public static int freq(int[] a, int target) {
+    int count = 0;
+    for (int i = 0; i <= a.length - 1; i++) {
+      if (a[i] == target) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /*
+  under construction
+  public static int freqR(int[] a, int target) {
+    int count = 0;
+    if (a[i] == target) {
+      count++;
+    }
+    return(count);
+  }
+  */
+
   public static void main(String[] args) {
     int[] a = new int[7];
-    int[] apple = {1,2,3,4,5};
+    int[] apple = {1,2,3,4,5,4,5,2,3,4,1,3};
     System.out.println(stringify(arrayGen(a)));
     System.out.println(linSearch(apple, 3));
+    System.out.println(linSearchR(apple, 3));
+    System.out.println(freq(apple, 4));
   }
 }
