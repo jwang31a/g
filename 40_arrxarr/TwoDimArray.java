f4ff741e0021fc75fe1b4baa5d11165add854f33
Your Name <you@example.com>
2021-11-29 21:12:03 -0500

    2

--- 40_arrxarr/TwoDimArray.java
+++ 40_arrxarr/TwoDimArray.java
@@ -28,67 +28,82 @@ public class TwoDimArray
 
   //postcond: prints each row of 2D int array a on its own line
   //          uses a FOREACH loop
-  public static void print2( int[][] a )
-  {
+  public static void print2( int[][] a ) {
     // YOUR IMPLEMENTATION HERE
     String output = "";
+    int counter = 0;
     for (int[] x: a) {
       for (int y: x) {
-        if (y == 0) {
+        if (counter == 0) {
           output += y;
         } else {
           output += ", " + y;
         }
-
+        counter += 1;
       }
+      counter = 0;
       output += "\n";
     }
     System.out.println(output);
   }
 
-
   //postcond: returns sum of all items in 2D int array a
-  /*
+
   public static int sum1( int[][] a )
   {
     // YOUR IMPLEMENTATION HERE
+    int output = 0;
+    int counter = 0;
+    for (int[] x: a) {
+      for (int y: x) {
+        output += y;
+      }
+    }
+    return output;
   }
-  */
+
 
 
   //postcond: returns sum of all items in 2D int array a
   //          * uses helper fxn sumRow
-  /*
+
   public static int sum2( int [][] m )
   {
     // YOUR IMPLEMENTATION HERE
+    
   }
-  */
+
 
   //postcond: returns sum of all items on row r of 2D int array a
   //          uses a FOR loop
-  /*
+
   public static int sumRow( int r, int[][] a )
   {
-    // YOUR IMPLEMENTATION HERE
+    int output = 0;
+    for (int i = 0; i < a[r].length; i++) {
+      output += a[r][i];
+    }
+    return output;
   }
-  */
+
 
 
   //postcond: returns sum of all items on row r of 2D int array a
   //          uses a FOREACH loop
-  /*
+
   public static int sumRow2(int r, int[][] m)
   {
     // *** YOUR IMPLEMENTATION HERE ***
     int summer = 0;
+    for (int y: m[r]) {
+      summer += y;
+    }
     return summer;
   }
-  */
 
 
-  public static void main( String [] args )
-  {
+
+  public static void main( String[] args ) {
       int [][] m1 = new int[4][2];
       int [][] m2 = { {2,4,6}, {3,5,7} };
       int [][] m3 = { {2}, {4,6}, {1,3,5} };
@@ -98,11 +113,11 @@ public class TwoDimArray
       print2(m1);
       print2(m2);
       print2(m3);
-      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       System.out.print("testing sum1...\n");
       System.out.println("sum m1 : " + sum1(m1));
       System.out.println("sum m2 : " + sum1(m2));
       System.out.println("sum m3 : " + sum1(m3));
+      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       System.out.print("testing sum2...\n");
       System.out.println("sum m1 : " + sum2(m1));
       System.out.println("sum m2 : " + sum2(m2));
@@ -110,6 +125,8 @@ public class TwoDimArray
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     // your own custom test cases welcomed and encouraged
+      System.out.println(sumRow(2,m3));
+      System.out.println(sumRow2(2,m3));
   }
 
 }//end class TwoDimArray
