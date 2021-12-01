9367cb8a548dff43e9c91d21cab7888527d62363
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-12-01 12:02:54 -0500

    Update Rational.java

--- 41_rational/Rational.java
+++ 41_rational/Rational.java
@@ -1,1 +1,33 @@
-
+public class Rational {
+  int n, d;
+  float r;
+  
+  public Rational() {
+    n = 0;
+    d = 1;
+    r = n / d;
+  }
+  
+  public Rational(int p, int q) {
+    n = p;
+    d = q;
+    r = p / q;
+  }
+  
+  public float floatValue() {
+    return r;
+  }
+  
+  public String toString() {
+    String str = p + "/" + q;
+    return str;
+  }
+  
+  /*
+  multiply
+  */
+  
+  public static void main(String[] args) {
+  
+  }
+}
