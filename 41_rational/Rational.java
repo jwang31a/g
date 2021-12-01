ea086c9ca0ba11f828decb51332113eb9b53fa9d
Your Name <you@example.com>
2021-12-01 17:24:27 -0500

    Rational

--- 41_rational/Rational.java
+++ 41_rational/Rational.java
@@ -1,6 +1,6 @@
 public class Rational {
   private int n, d;
-  private float r;
+  private double r;
 
   public Rational() {
     n = 0;
@@ -11,16 +11,16 @@ public class Rational {
   public Rational(int p, int q) {
     if (q == 0) {
       n = 0;
-      q = 1;
-      System.out.println("Invalid denominator of 0, number's value set to 0/1.")
+      d = 1;
+      System.out.println("Invalid denominator of 0, number's value set to 0/1.");
     } else {
       n = p;
       d = q;
     }
-    r = (float)p / (float)q;
+    r = (double)p / (double)q;
   }
 
-  public float floatValue() {
+  public double floatValue() {
     return r;
   }
 
@@ -45,14 +45,18 @@ public class Rational {
   }
 
   public void divide(Rational input) {
-    n = n * input.getD();
-    d = d * input.getN();
-    r = n / d;
+    if (input.getN() == 0 || n == 0) {
+      System.out.println("Division by 0 error, invalid numerator");
+    } else {
+      n = n * input.getD();
+      d = d * input.getN();
+      r = n / d;
+    }
   }
 
   public static void main(String[] args) {
-    //creation of two rational numbers (instances of class Rational)
-    Rational testing = new Rational(4, 3);
+    //creation of two rational numbers (instances of class Rational), test calls to test methods
+    Rational testing = new Rational(4, 0);
     Rational asdf = new Rational(16, 9);
 
     //to test out original values of asdf
@@ -60,6 +64,8 @@ public class Rational {
     System.out.println(asdf.toString());
     System.out.println(asdf.getN());
     System.out.println(asdf.getD());
+    System.out.println(testing.getN());
+    System.out.println(testing.getD());
 
     asdf.multiply(testing);
     //to test out values of asdf after multply and divide
