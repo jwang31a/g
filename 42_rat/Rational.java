b000340624b2f586f1c86986748b3b371988564b
Your Name <you@example.com>
2021-12-05 20:15:07 -0500

    rat

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -8,9 +8,11 @@ Time Spent:  hours
 DISCO:
 Floating point math in java is weird, since two numbers that should be the same might not be represented the same.
 For example, if we are comparing 0.333333 and 0.3333334, they're pretty similar (and in some cases just different representations of the same number), but java will think they're different.
+We compared the rational numbers 2/3 and 6/9, and they were not equal (in the if statement).
 
 QCC:
-How can we make a functional version of compareTo?
+How can we make a more functional version of compareTo?
+It works right now, but we had to include a failsafe return statement, otherwise the code wouldn't compile.
 */
 
 public class Rational {
@@ -112,25 +114,27 @@ public class Rational {
     this.d = this.d / g;
   }
 
-  //doesn't work, missing return statement, not sure what's happening here
-  //i'm thinking it could be something related to floating-point numbers and figuring out which is larger
-  //i think we shouldn't use floating points for this, because == means every digit has to be the same, and that might not be possible with floating point numbers
+  //this method kind of works
+  //the last return statement shouldn't be there, but I have to include it otherwise the code will not compile
+  //it's kind of to cover the case where the two numbers are equal, since floating point math and comparisons are weird
   public int compareTo(Rational input) {
     //this part of the code doesn't work, since 2/3 and 6/9 are represented differently
-    if (this.r > input.r + 0.05 && this.r< input.r + 0.05) {
+    if (this.r < input.r + 0.05 && this.r > input.r - 0.05) {
       return 0;
     } else if (this.r < input.r) {
       return -1;
     } else if (this.r > input.r) {
       return 1;
     }
-    return(1234);
+    //this return statement has to be here because the if statement doesn't fully work
+    //the if else statements work, but not the if
+    return(0);
   }
   //end of new code
 
   public static void main(String[] args) {
     Rational r = new Rational(2,3); //Stores the rational number 2/3
-    Rational s = new Rational(6,9); //Stores the rational number 1/2
+    Rational s = new Rational(1,2); //Stores the rational number 1/2
     Rational t = new Rational(4,18); //Stores the rational number 4/18
     r.add(s);  //Adds r to s, changes r to 7/6.  s remains 1/2
     System.out.println(r.toString());
@@ -143,6 +147,6 @@ public class Rational {
     t.reduce(); //Changes t to 2/9
     System.out.println(t.toString());
 
-    System.out.println("\n" + r.compareTo(s));
+    System.out.println("\n" + r.compareTo(s)); //0 means the numbers are (approximately) equal, 1 means r is bigger, -1 means s is bigger
   }
 }
