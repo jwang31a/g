cb0466e201dda4b5cdc8a75b576feaceaec260b4
jwang31a <90936863+jwang31a@users.noreply.github.com>
2021-12-23 11:59:08 -0500

    Create Driver.java

---
+++ 49_rat/Driver.java
@@ -0,0 +1,15 @@
+public class Driver{
+  public static void main(String[] args){
+    Rational x = new Rational(1,2);
+    Rational y = new Rational(2,4);
+    Rational z = new Rational(1,3);
+    Integer w = 12;
+    System.out.println(x.compareTo(y)); // Equal
+    System.out.println(y.compareTo(z));
+    System.out.println(z.compareTo(y));
+    //System.out.println(z.compareTo(w));
+    System.out.println(z.equals(x));
+    System.out.println(x.equals(y));
+    System.out.println(x.equals(z));
+  }
+}
