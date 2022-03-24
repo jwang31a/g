5a87ac0ce88926d2e203d10ebbcc24eb69ea4592
Jun Hong Wang <jwang31@stuy.edu>
2022-03-24 12:28:20 -0400

    archive

--- 00_fall/09_bigsib/Greet.java
+++
@@ -1,9 +0,0 @@
-public class Greet {
-	public static void main(String[] args) {
-		BigSib.greet("Flim");
-		BigSib.greet("Flam");
-		BigSib.greet("Kazaam");
-		// When I was testing this, I'm not sure why it didn't work, but then it suddenly worked.
-		// I don't know what I changed, but I made this part work somehow.
-	}
-}
--- 45_suparr/old/OldListInt.java
+++
@@ -1,6 +0,0 @@
-public interface OldListInt {
-  public void add(int newVal);
-  public void add(int index, int NewVal);
-  public void remove(int index);
-  public int size();
-}
