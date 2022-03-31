eae119caa0109f8cc61a75ae537b51b26ef1874c
Your Name <you@example.com>
2022-03-31 18:04:25 -0400

    scheming

--- 86_schemery/Scheme.java
+++ 86_schemery/Scheme.java
@@ -1,11 +1,25 @@
+/*
+Blonde Himbos: Hugo Jenkins + Boary, Micheal Kamela, Jun Hong Wang
+APCS pd6
+HW86 - What a Racket
+2022-03-31
+time spent: h
+*/
+
 /***
  * class Scheme
  * Simulates a rudimentary Scheme interpreter
  *
  * ALGORITHM for EVALUATING A SCHEME EXPRESSION:
  *   1. Steal underpants.
- *   2. ...
- *   5. Profit!
+ *   2. Parse through String, turn it into an array of single character Strings.
+ *   3. Declare 2 stacks, one for parentheses, and one for operations.
+ *   4. If string contains parentheses or integers, push onto Stack 1, else, push onto stack 2.
+ *   5. If we reach a closing parentheses, we don't add it, instead, we pop everything until we reach an opening parentheses.
+ *   6. We pop the operation stored in Stack 2, and perform it on the integers we popped off from Stack 1.
+ *   7. Push onto the stack the result of that operation, and continue, until the Stack contains one value only, which is the final value.
+ *   8. Return this final value as a String.
+ *   9. Profit!
  *
  * STACK OF CHOICE: ____ by ____
  * b/c ...
@@ -15,7 +29,7 @@ public class Scheme
 {
   /***
    * precond:  Assumes expr is a valid Scheme (prefix) expression,
-   *           with whitespace separating all operators, parens, and 
+   *           with whitespace separating all operators, parens, and
    *           integer operands.
    * postcond: Returns the simplified value of the expression, as a String
    * eg,
