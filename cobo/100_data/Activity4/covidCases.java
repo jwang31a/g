6b18f25e8f4630d854faf94553874e460838250f
Your Name <you@example.com>
2022-04-06 16:36:05 -0400

    broken

--- cobo/100_data/Activity4/covidCases.java
+++ cobo/100_data/Activity4/covidCases.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class covidCases {
    public static void main(String[] args) {
-      DataSource ds = DataSource.connect("https://www.kaggle.com/datasets/imdevskp/corona-virus-report").load();
-      ArrayList<covidCases> allstns = ds.fetchList("Country/Region", "Confirmed", "Recovered", "Deaths");
+      DataSource ds = DataSource.connect("https://www.kaggle.com/datasets/imdevskp/corona-virus-report"); //.load();
+      ArrayList<covid> allstns = ds.fetchList("Country/Region", "Confirmed", "Recovered", "Deaths");
       System.out.println("Total countries " + allstns.size());
 
       Scanner sc = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class covidCases {
       System.out.println("Number of confirmed cases in " + userCountry);
 
 
-      for (covidCases ws : allstns) {
+      for (covid ws : allstns) {
          if (ws.getCountry().equals(userCountry)) {
             System.out.println("  " + ws.getConfirmed());
          }
